76190b95eae3ea0be3787e729b157bfbf4e7c838
alexburroughs <1628499@johnabbottcollege.net>
2019-06-21 21:18:10 -0400

    deleted duplicates

--- comp-test/target/debug/deps/comp_test-bba5fbff8c37bd00.d
+++
@@ -1,6 +0,0 @@
-c:\Users\Alex\Documents\GitHub\comp-test\comp-test\target\debug\deps\comp_test-bba5fbff8c37bd00.rmeta: src\main.rs src\filemanager\mod.rs
-
-c:\Users\Alex\Documents\GitHub\comp-test\comp-test\target\debug\deps\comp_test-bba5fbff8c37bd00.d: src\main.rs src\filemanager\mod.rs
-
-src\main.rs:
-src\filemanager\mod.rs:
