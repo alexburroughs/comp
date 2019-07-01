aedc0d02459626e0322c090f6ed0acbfbac33aa4
alexburroughs <1628499@johnabbottcollege.net>
2019-06-30 23:01:00 -0400

    update folders

--- comp/target/rls/debug/deps/comp_test-cd606d6c9da29bac.d
+++
@@ -1,6 +0,0 @@
-c:\Users\Alex\Documents\GitHub\comp\comp\target\rls\debug\deps\comp_test-cd606d6c9da29bac.rmeta: src\main.rs src\filemanager\mod.rs
-
-c:\Users\Alex\Documents\GitHub\comp\comp\target\rls\debug\deps\comp_test-cd606d6c9da29bac.d: src\main.rs src\filemanager\mod.rs
-
-src\main.rs:
-src\filemanager\mod.rs:
