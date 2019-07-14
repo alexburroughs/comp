f76a2b0c9ce98659ac89087a94747ae9afe52880
Alex Burroughs <aburroughs00@gmail.com>
2019-07-14 12:40:15 -0400

    released and added documentation

---
+++ vm/target/debug/deps/vm-264d94e53c2705a3.d
@@ -0,0 +1,11 @@
+/home/alex/Documents/github/comp/vm/target/debug/deps/vm-264d94e53c2705a3: src/main.rs src/filemanager/mod.rs src/runtime/vm.rs src/runtime/vm/data.rs src/runtime/vm/system.rs src/runtime/command.rs src/tokenizer/tokenizer.rs
+
+/home/alex/Documents/github/comp/vm/target/debug/deps/vm-264d94e53c2705a3.d: src/main.rs src/filemanager/mod.rs src/runtime/vm.rs src/runtime/vm/data.rs src/runtime/vm/system.rs src/runtime/command.rs src/tokenizer/tokenizer.rs
+
+src/main.rs:
+src/filemanager/mod.rs:
+src/runtime/vm.rs:
+src/runtime/vm/data.rs:
+src/runtime/vm/system.rs:
+src/runtime/command.rs:
+src/tokenizer/tokenizer.rs:
---
+++ vm/target/release/deps/vm-e2499450acf31130.d
@@ -0,0 +1,11 @@
+/home/alex/Documents/github/comp/vm/target/release/deps/vm-e2499450acf31130: src/main.rs src/filemanager/mod.rs src/runtime/vm.rs src/runtime/vm/data.rs src/runtime/vm/system.rs src/runtime/command.rs src/tokenizer/tokenizer.rs
+
+/home/alex/Documents/github/comp/vm/target/release/deps/vm-e2499450acf31130.d: src/main.rs src/filemanager/mod.rs src/runtime/vm.rs src/runtime/vm/data.rs src/runtime/vm/system.rs src/runtime/command.rs src/tokenizer/tokenizer.rs
+
+src/main.rs:
+src/filemanager/mod.rs:
+src/runtime/vm.rs:
+src/runtime/vm/data.rs:
+src/runtime/vm/system.rs:
+src/runtime/command.rs:
+src/tokenizer/tokenizer.rs:
